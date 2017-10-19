cc5e36a246458ecb62db0d98642b5b104be101db
Yi Xiao <yi.xiao@student.unsw.edu.au>
2017-10-19 17:31:20 +1100

    modified plotting shell script

--- ass2/vp_plot.sh
+++ ass2/vp_plot.sh
@@ -1,11 +1,21 @@
 #!/usr/bin/sh
 
-for rate in {1..5}
+
+for mode in SHP SDP LLP
 do
-    for mode in SHP SDP LLP
+    echo "================================="
+    echo "$mode"
+    for rate in {1..5}
     do
-        echo "================================="
-        echo "$mode: $rate"
-        ./RoutingPerformance.py PACKET $mode topology.txt workload.txt $rate | sed -n -e 4p -e 7,8p | cut -d: -f2 | tr -d ' '  
+        array=( $(./RoutingPerformance.py PACKET $mode topology.txt workload.txt $rate | sed -n -e 4p -e 7,8p | cut -d: -f2 | tr -d ' ') )
+        SUCCESS[$rate-1]=${array[0]}
+        HOP[$rate-1]=${array[1]}
+        DELAY[$rate-1]=${array[2]}
     done
+    echo "SUCCESS:"
+    printf '%s\n' "${SUCCESS[@]}"
+    echo "HOP:"
+    printf '%s\n' "${HOP[@]}"
+    echo "DELAY:"
+    printf '%s\n' "${DELAY[@]}"
 done
